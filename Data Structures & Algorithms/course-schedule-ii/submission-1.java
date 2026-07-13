8a29776366d335c982b17365cb9498c1d67b459c
Gaurav Joshi <66788092+gaurav20199@users.noreply.github.com>
2026-07-14 00:34:32 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int idx=0;idx<=numCourses;idx++)
+            graph.add(new ArrayList<>());
+
+        for(int []prerequisite:prerequisites) {
+            graph.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        boolean []visited = new boolean[graph.size()+1];
+        boolean []dfsVisited = new boolean[graph.size()+1];
+        Stack<Integer> dependencyOrder = new Stack<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i] && hasCycle(graph, visited, dfsVisited, i,dependencyOrder))
+                return new int[0];
+        }
+
+        int []res = new int[dependencyOrder.size()];
+        //int idx = dependencyOrder.size()-1;
+        int idx = 0;
+        while(!dependencyOrder.isEmpty()) {
+            res[idx++] = dependencyOrder.pop();
+        }
+        return res;
+    }
+
+    public boolean hasCycle(List<List<Integer>> graph, boolean []visited, boolean []dfsVisited, int src,Stack<Integer> dependencyOrder) {
+        if (dfsVisited[src]) 
+            return true;
+        if (visited[src]) 
+            return false;
+
+        dfsVisited[src] = true;
+        visited[src] = true;       
+        for(int nbr:graph.get(src)) {
+            if(hasCycle(graph,visited,dfsVisited,nbr,dependencyOrder))
+                return true;
+        }
+        dfsVisited[src] = false;
+        dependencyOrder.push(src); 
+        return false;
+    }
+
+        
+}
+    
+

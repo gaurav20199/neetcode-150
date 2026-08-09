d6da49bb01c0a7dfafb739f0dde420810efd7fb9
Gaurav Joshi <66788092+gaurav20199@users.noreply.github.com>
2026-08-10 01:59:31 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    class Pair implements Comparable<Pair> {
+        int position;
+        int time;
+
+        public Pair(int position, int time) {
+            this.position = position;
+            this.time = time;
+        }
+
+        public int compareTo(Pair other) {
+            return other.position-this.position;
+        }
+    }
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<Pair> pairs = new ArrayList<>();
+        for(int idx=0;idx<position.length;idx++) {
+            pairs.add(new Pair(position[idx],speed[idx]));
+        }
+        Collections.sort(pairs);
+        Stack<Double> stack = new Stack<>();
+        for(Pair pair: pairs) {
+            stack.push((double) (target - pair.position) / pair.time);
+            while(stack.size()>=2 && stack.peek()<=stack.get(stack.size()-2)) {
+                stack.pop();
+            }
+
+        }
+        return stack.size();
+    }
+}
